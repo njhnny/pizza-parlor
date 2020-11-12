d9a7768beb466a47e221ea445cb9439618d87499
Nick Hennessy <njhnny@gmail.com>
2020-11-11 20:37:51 -0800

    add variables for price adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,21 @@ this.size = size;
 this.toppings = toppings;
 this.price = price;
 };
-
+//variables to adjust base cost and price per topping
+baseCost = 10;
+perTopping = 1;
+//add price of toppings to total
 Pizza.prototype.toppingsPrice = function() {
-  this.price = ((this.toppings.length) + this.price);
+  this.price = (((this.toppings.length) * perTopping) + this.price);
 };
-
+//modify price by pizza size
 Pizza.prototype.sizePrice = function() {
   if (this.size === "medium") {
     this.price = (2 + this.price);
   } else if (this.size === "large") {
     this.price = (5 + this.price);
-  }
+  };
 };
-
 //ui logic
 $(document).ready(function() {
   $("form.form").submit(function(event) {
@@ -26,7 +28,7 @@ $(document).ready(function() {
     $(".toppings:checked").each(function(){
       toppingsPick.push($(this).val());
   });
-  let pizzaObject = new Pizza(sizePick, toppingsPick, 10);
+  let pizzaObject = new Pizza(sizePick, toppingsPick, baseCost);
   console.log(pizzaObject);
   pizzaObject.toppingsPrice();
   pizzaObject.sizePrice();
