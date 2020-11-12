6420d674b5647183f15baf002e877d0a6fe87d5a
Nick Hennessy <njhnny@gmail.com>
2020-11-11 18:36:46 -0800

    assign inputs to pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 //business logic
-function Pizza() {
-this.size = "";
-this.toppings = [];
-this.price = 0;
+function Pizza(size, toppings, price) {
+this.size = size;
+this.toppings = toppings;
+this.price = price;
 };
 
 //ui logic
 $(document).ready(function() {
-
+  $("form.form").submit(function(event) {
+    event.preventDefault();
+    const sizePick = $("#size").val();
+    const toppingsPick = [];
+    $(".toppings:checked").each(function(){
+      toppingsPick.push($(this).val());
+  });
+  let pizzaObject = new Pizza(sizePick, toppingsPick, 10);
+  console.log(pizzaObject);
+  });
 });
